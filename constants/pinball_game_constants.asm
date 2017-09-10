6663f3086c777953eb857429573057b00cd46e84
chaos-lord <dd4791.mariokarter@hotmail.co.uk>
2017-09-10 22:20:14 +0100

    reduced logging space to account for other changes

--- constants/pinball_game_constants.asm
+++ constants/pinball_game_constants.asm
@@ -16,4 +16,4 @@ MAX_BONUS_MULTIPLIER_FIELD_EVENTS EQU 11
 MAP_MOVE_FRAMES_COUNTER        EQU  480 ; 8 seconds until the map move counter decrease by 1
 PINBALL_UPGRADE_FRAMES_COUNTER EQU 3600 ; ~1 minute until the pinball type degrades one stage. (e.g. Great Ball -> Pokeball)
 
-LOGGING_SPACE EQU 185
+LOGGING_SPACE EQU 184

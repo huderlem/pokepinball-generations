58de729ba614ca2a31e1badb4600380028e9932c
Marcus Huderle <huderlem@gmail.com>
2018-04-21 11:46:32 -0700

    Set wild mon data for gold/silver tables

--- data/wild_mons.asm
+++ data/wild_mons.asm
@@ -39,33 +39,37 @@ BlueStageWildMonsPointers:
 	db $FF ; terminator
 
 GoldStageWildMonsPointers:
-	dbw PALLET_TOWN,       RedStagePalletTownWildMons
-	dbw VIRIDIAN_FOREST,   RedStageViridianForestWildMons
-	dbw PEWTER_CITY,       RedStagePewterCityWildMons
-	dbw CERULEAN_CITY,     RedStageCeruleanCityWildMons
-	dbw VERMILION_SEASIDE, RedStageVermilionSeasideWildMons
-	dbw ROCK_MOUNTAIN,     RedStageRockMountainWildMons
-	dbw LAVENDER_TOWN,     RedStageLavenderTownWildMons
-	dbw CYCLING_ROAD,      RedStageCyclingRoadWildMons
-	dbw SAFARI_ZONE,       RedStageSafariZoneWildMons
-	dbw SEAFOAM_ISLANDS,   RedStageSeafoamIslandsWildMons
-	dbw CINNABAR_ISLAND,   RedStageCinnabarIslandWildMons
-	dbw INDIGO_PLATEAU,    RedStageIndigoPlateauWildMons
+	dbw NEW_BARK_TOWN,   GoldStageNewBarkTownMons
+	dbw VIOLET_CITY,     GoldStageVioletCityWildMons
+	dbw RUINS_OF_ALPH,   GoldStageRuinsOfAlphWildMons
+	dbw LAKE_OF_RAGE,    GoldStageLakeOfRageWildMons
+	dbw ECRUTEAK_CITY,   GoldStageEcruteakCityWildMons
+	dbw ILEX_FOREST,     GoldStageIlexForestWildMons
+	dbw GOLDENROD_CITY,  GoldStageGoldenrodCityWildMons
+	dbw OLIVINE_CITY,    GoldStageOlivineCityWildMons
+	dbw ICE_PATH,        GoldStageIcePathWildMons
+	dbw DARK_CAVE,       GoldStageDarkCaveWildMons
+	dbw BURNED_TOWER,    GoldStageBurnedTowerWildMons
+	dbw TIN_TOWER,       GoldStageTinTowerWildMons
+	dbw BLACKTHORN_CITY, GoldStageBlackthornCityWildMons
+	dbw MT_SILVER,       GoldStageMtSilverWildMons
 	db $FF ; terminator
 
 SilverStageWildMonsPointers:
-	dbw VIRIDIAN_CITY,     BlueStageViridianCityWildMons
-	dbw VIRIDIAN_FOREST,   BlueStageViridianForestWildMons
-	dbw MT_MOON,           BlueStageMtMoonWildMons
-	dbw CERULEAN_CITY,     BlueStageCeruleanCityWildMons
-	dbw VERMILION_STREETS, BlueStageVermilionStreetsWildMons
-	dbw ROCK_MOUNTAIN,     BlueStageRockMountainWildMons
-	dbw CELADON_CITY,      BlueStageCeladonCityWildMons
-	dbw FUCHSIA_CITY,      BlueStageFuchsiaCityWildMons
-	dbw SAFARI_ZONE,       BlueStageSafariZoneWildMons
-	dbw SAFFRON_CITY,      BlueStageSaffronCityWildMons
-	dbw CINNABAR_ISLAND,   BlueStageCinnabarIslandWildMons
-	dbw INDIGO_PLATEAU,    BlueStageIndigoPlateauWildMons
+	dbw NEW_BARK_TOWN,   SilverStageNewBarkTownMons
+	dbw VIOLET_CITY,     SilverStageVioletCityWildMons
+	dbw DARK_CAVE,       SilverStageDarkCaveWildMons
+	dbw MAHOGANY_TOWN,   SilverStageMahoganyTownWildMons
+	dbw AZALEA_TOWN,     SilverStageAzaleaTownWildMons
+	dbw ILEX_FOREST,     SilverStageIlexForestWildMons
+	dbw NATIONAL_PARK,   SilverStageNationalParkWildMons
+	dbw OLIVINE_CITY,    SilverStageOlivineCityWildMons
+	dbw ICE_PATH,        SilverStageIcePathWildMons
+	dbw MT_MORTAR,       SilverStageMtMortarWildMons
+	dbw BURNED_TOWER,    SilverStageBurnedTowerWildMons
+	dbw WHIRL_ISLANDS,   SilverStageWhirlIslandsWildMons
+	dbw BLACKTHORN_CITY, SilverStageBlackthornCityWildMons
+	dbw MT_SILVER,       SilverStageMtSilverWildMons
 	db $FF ; terminator
 
 INCLUDE "data/red_wild_mons.asm"
